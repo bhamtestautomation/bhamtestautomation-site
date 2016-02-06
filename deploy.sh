a47f8d80b4b6073d680f723430f00e473feaa335
Jason <jason@scurker.com>
2016-02-05 21:16:07 -0600

    fix deploy task

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,13 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
-# Clone *.github.io repo to build folder
-git clone https://github.com/bhamtestautomation/bhamtestautomation.github.io.git build > /dev/null 2>&1
-rm -rf build/*
+# Clone *.github.io repo to staging folder
+git clone https://github.com/bhamtestautomation/bhamtestautomation.github.io.git stage > /dev/null 2>&1
+rm -rf stage/*
 
-# Build latest commit
-npm run build && cd build
+# Build latest commit and copy to stage
+npm run build && mv build/* stage && cd stage
 
-git rev-parse HEAD
 git add --all .
 git commit -m "Deploying ${TRAVIS_COMMIT} to GitHub pages"
 git push origin master --quiet > /dev/null 2>&1

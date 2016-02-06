a62e79df8c347e326ebd2eefd5b9b2184c23e2ba
Jason <jason@scurker.com>
2016-02-05 21:20:50 -0600

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
+if [ "${TRAVIS}" = "true" ]; then
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+fi
 
 # Clone *.github.io repo to staging folder
 git clone https://github.com/bhamtestautomation/bhamtestautomation.github.io.git stage > /dev/null 2>&1
@@ -14,6 +16,6 @@ npm run build && mv build/* stage && cd stage
 
 git add --all .
 git commit -m "Deploying ${TRAVIS_COMMIT} to GitHub pages"
-git push origin master --quiet > /dev/null 2>&1
+git push origin master
 
 fi
